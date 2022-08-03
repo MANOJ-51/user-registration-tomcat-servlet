5375a39ca3eee64f15db63520058d64fb091f13e
MANOJ-51 <m4anoj51@gmail.com>
2022-08-03 22:21:11 +0530

    added: method to add regex to the user name

--- main/java/com/bridgelabz/LoginServlet.java
+++ main/java/com/bridgelabz/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -27,13 +28,13 @@ public class LoginServlet extends HttpServlet {
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
-        if (userId.equals(user) && password.equals(pwd)) {
+        if (Pattern.matches("[A-Z][a-z]{2,}", user) && userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Login.html");
             PrintWriter output = response.getWriter();
-            output.println("<font colour=red>Either user name or password is wrong.</font>");
+            output.println("<font color=red>Either user name or password is wrong.</font>");
             requestDispatcher.include(request, response);
         }
     }
